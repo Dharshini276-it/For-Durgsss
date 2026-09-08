168ab9734ec82fab639866472da410e9aaf3d715
Dharshini276-it <dharshini.a.2026@gmail.com>
2026-09-08 23:44:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function goNext() {
+    window.location.href = "page2.html";
+}
+
+function showMessage() {
+    window.location.href = "page3.html";
+}
+function goPage4() {
+    window.location.href = "page4.html";
+}
+function openLetter() {
+
+    const page = document.querySelector(".letter-page");
+
+    page.classList.add("open");
+
+}
+
+
+function goPage5() {
+    window.location.href = "page5.html";
+}
+function goPage5() {
+    window.location.href = "page5.html";
+}
